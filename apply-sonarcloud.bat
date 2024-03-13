bec3f346ff04732ae20deff5a46285271dfe3b79
voomdoon <nilreb24-github20141105@yahoo.de>
2024-03-13 06:13:20 +0100

    feat: add apply-sonarcloud

---
+++ apply-sonarcloud.bat
@@ -0,0 +1,12 @@
+SET mypath=%~dp0
+SET mypath=%mypath:~0,-1%
+
+CD %1
+	MKDIR "%1\.github"
+		MKDIR "%1\.github\workflows"
+			CALL copy "%mypath%\template\.github\workflows\sonarcloud.yml" "%1\.github\workflows\sonarcloud.yml" /Y
+				ECHO "replace 'PROJECT_ID' by '%~n1'"
+				CALL powershell -Command "(gc .github\workflows\sonarcloud.yml) -replace 'PROJECT_ID', '%~n1' | Out-File -encoding utf8 .github\workflows\sonarcloud.yml"
+				CALL git add .github/workflows/sonarcloud.yml
+				CALL git commit -m "build(ci): update sonarcloud"
+CD %mypath%
