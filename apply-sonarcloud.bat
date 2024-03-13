5247c0d8975e3cf46888d6331054ade62af22443
voomdoon <nilreb24-github20141105@yahoo.de>
2024-03-13 06:17:44 +0100

    fix: copy of workflow file

--- apply-sonarcloud.bat
+++ apply-sonarcloud.bat
@@ -5,7 +5,7 @@ SET currentdir=%CD%
 CD %1
 	MKDIR "%1\.github"
 		MKDIR "%1\.github\workflows"
-			CALL copy "%mypath%\template\.github\workflows\sonarcloud.yml" "%1\.github\workflows\sonarcloud.yml" /Y
+			CALL copy "%mypath%\template\.github\workflows\sonarcloud.yml" ".github\workflows\sonarcloud.yml" /Y
 				ECHO "replace 'PROJECT_ID' by '%~n1'"
 				CALL powershell -Command "(gc .github\workflows\sonarcloud.yml) -replace 'PROJECT_ID', '%~n1' | Out-File -encoding utf8 .github\workflows\sonarcloud.yml"
 				CALL git add .github/workflows/sonarcloud.yml
